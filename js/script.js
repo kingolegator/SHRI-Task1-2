08959348f4e2f056782237500c722e7b1b994c93
Олег <oleg@MacBook-Pro-Oleg.local>
2018-10-08 03:01:06 +0300

    feature: rotate gesture for setting brightness on webcam image

--- js/script.js
+++ js/script.js
@@ -1,42 +1,35 @@
 //#region poinerEvents
 
-var evCache = new Array();
-var prevDiff = -1;
-
+let evCache = new Array();
+let multiTouchPointerEvents = new Array();
+let prevDiff = -1;
 let currentGesture = null;
-
-const webcam = document.getElementById('webcam');
-
-let nodeState = {
-    imageWidth: webcam.x,
+let transformState = {
     scale: 1.8,
-    lastX: 0
+    lastPosition: 0
 };
 
-webcam.addEventListener('pointerdown', pointDown);
-webcam.addEventListener('pointerup', cancelTouch);
-webcam.addEventListener('pointercancel', cancelTouch);
-webcam.addEventListener('pointermove', move);
+const webcam = document.getElementById('webcam');
+const _maxValues = {
+    imageWidth: webcam.width,
+    minScale: 1,
+    maxScale: 5,
+    maxBrightness: 1,
+    minBrightness: 0
+};
+
+webcam.onpointerdown = pointDown;
+webcam.onpointerup = cancelTouch;
+webcam.onpointercancel = cancelTouch;
+webcam.onpointermove = move;
+webcam.onpointerout = cancelTouch;
+webcam.onpointerleave = cancelTouch;
 webcam.onpointerout = cancelTouch;
 webcam.onpointerleave = cancelTouch;
 
 function move(event) {
-    if (!currentGesture) {
-        return
-    }
+
     event.preventDefault();
-    console.log("move", event);
-
-    const { startX } = currentGesture;
-    const { x } = event;
-
-    if (evCache.length == 1) {
-        const dx = x - startX;
-        webcam.style.transform = `
-            translateX(${nodeState.lastX + dx}px)
-            scale(${nodeState.scale})`;
-        nodeState.lastX = dx;
-    }
 
     for (var i = 0; i < evCache.length; i++) {
         if (event.pointerId == evCache[i].pointerId) {
@@ -45,27 +38,68 @@ function move(event) {
         }
     }
 
-    if (evCache.length == 2) {
-        let curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
-        if (prevDiff > 0) {
-            if (curDiff > prevDiff) {
-                nodeState.scale *= curDiff / prevDiff;
-                webcam.style.transform = `translateX(${nodeState.lastX}px) scale(${nodeState.scale})`;
-            }
-            if (curDiff < prevDiff) {
-                nodeState.scale *= curDiff / prevDiff;
-                webcam.style.transform = `translateX(${nodeState.lastX}px) scale(${nodeState.scale})`;
+    if (evCache.length == 1) {
+        if (!currentGesture)
+            return;
+        const { x } = event;
+        const dx = x - currentGesture.startX;
+        if ((((webcam.offsetWidth * transformState.scale) - webcam.offsetWidth) / (transformState.scale * 2) * transformState.scale) > Math.abs(dx + transformState.lastPosition)) {
+            webcam.style.transform = `
+                translateX(${dx + transformState.lastPosition}px)
+                scale(${transformState.scale})`;
+            webcam.style.webKitTransform = `
+                translateX(${dx + transformState.lastPosition}px)
+                scale(${transformState.scale})`;
+        }
+    }
+
+    else if (evCache.length == 2) {
+        let curDiffX = Math.abs(evCache[0].clientX - evCache[1].clientX);
+        let curDiffY = Math.abs(evCache[0].clientY - evCache[1].clientY);
+        let diffCtn = Math.atan2(curDiffY, curDiffX) * 180 / Math.PI;
+        let dropBeetwPoints = Math.sqrt(Math.pow(curDiffX, 2) * 2 + Math.pow(curDiffY, 2) * 2);
+
+        multiTouchPointerEvents.push({ drop: dropBeetwPoints.toFixed(6), angle: diffCtn.toFixed(6) });
+        if (multiTouchPointerEvents.length >= 2) {
+            switch (true) {
+                case (Math.abs(diffCtn - multiTouchPointerEvents[0].angle) > 15 &&
+                    Math.abs(dropBeetwPoints - multiTouchPointerEvents[multiTouchPointerEvents.length - 2].drop) < 25):
+                    webcam.style.filter = `brightness(${(Math.abs(diffCtn - multiTouchPointerEvents[0].angle)) / 20})`;
+                    webcam.style.webkitFilter = `brightness(${(Math.abs(diffCtn - multiTouchPointerEvents[0].angle)) / 20})`;
+                    light.innerText = `Яркость: ${Math.round(Math.abs(diffCtn - multiTouchPointerEvents[0].angle) * 1.1)}%`;
+                    console.log("rotate");
+                    break;
+                default:
+                    if (prevDiff > 0) {
+                        if (curDiffX > prevDiff && ((transformState.scale * (curDiffX / prevDiff)) < _maxValues.maxScale)) {
+                            transformState.scale *= curDiffX / prevDiff;
+                            webcam.style.transform = `translateX(${transformState.scale < 2 ? 0 : transformState.lastPosition}px) scale(${transformState.scale})`;
+                            webcam.style.webKitTransform = `translateX(${transformState.scale < 2 ? 0 : transformState.lastPosition}px) scale(${transformState.scale})`;
+                        }
+                        if (curDiffX < prevDiff && ((transformState.scale * (curDiffX / prevDiff)) > _maxValues.minScale)) {
+                            transformState.scale *= curDiffX / prevDiff;
+                            webcam.style.transform = `translateX(${transformState.scale < 2 ? 0 : (transformState.lastPosition / transformState.scale)}px) scale(${transformState.scale})`;
+                            webcam.style.webKitTransform = `translateX(${transformState.scale < 2 ? 0 : (transformState.lastPosition / transformState.scale)}px) scale(${transformState.scale})`;
+                        }
+                    }
+                    console.log("pinch");
+                    zoom.innerText = `Приближение: ${Math.round(transformState.scale * 100) - 100}%`;
+                    prevDiff = curDiffX;
+                    break;
             }
         }
-        prevDiff = curDiff;
     }
 }
 
 function cancelTouch(event) {
     console.log("up", event);
     currentGesture = null;
+    transformState.lastPosition = !!webcam.style.transform ? Number(webcam.style.transform.split('scale')[0].split('(')[1].split('px)')[0]) : 0;
     remove_event(event);
-    if (evCache.length < 2) prevDiff = -1;
+    if (evCache.length < 2) {
+        prevDiff = -1;
+        multiTouchPointerEvents = [];
+    }
 }
 
 function pointDown(event) {
@@ -73,13 +107,13 @@ function pointDown(event) {
     evCache.push(event);
     webcam.setPointerCapture(event.pointerId);
     currentGesture = {
-        startX: event.x,
+        startX: event.x
     };
 }
 
-function remove_event(ev) {
+function remove_event(event) {
     for (var i = 0; i < evCache.length; i++) {
-        if (evCache[i].pointerId == ev.pointerId) {
+        if (evCache[i].pointerId == event.pointerId) {
             evCache.splice(i, 1);
             break;
         }
